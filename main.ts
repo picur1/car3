aad03f7c557ec0e4c3a28f8511c35b749feb55ab
picur1 <133989406+picur1@users.noreply.github.com>
2023-07-18 10:45:29 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "N") {
+        RingbitCar.running_distance(RingbitCar.Direction_run.forward, value)
+    }
+    if (name == "R") {
+        RingbitCar.steering_angle(RingbitCar.Direction_turn.left, value)
+    }
+})
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
